2acdf132222a3af8cd3aa688c42e43f251bb075b
nshin7 <namishin@ucla.edu>
2023-05-12 13:42:35 -0700

    TRIED AGAIN

--- Example_Script.R
+++ Example_Script.R
@@ -7,3 +7,11 @@ head(iris)
 df <- read.csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/static/tt_data_type.csv")
 head(df)
 df
+
+
+colnames(df)
+
+## Added more. 
+
+colnames(df)
+table(df$data_type)
